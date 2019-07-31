0674f8b18a80b6678500c5d2b117daeb48b8825c
himanish58 <himanish.kabiraj@gmail.com>
2019-08-01 03:03:39 +0530

    Cleanup and final output

--- src/App.jsx
+++ src/App.jsx
@@ -19,11 +19,23 @@ class App extends Component {
 		this.styles = {
 			textField: {
 				width: '40%'
+			},
+			submitButton: {
+				background: '#0000ff',
+				color: '#ffffff'
+			},
+			disabledSubmitButton: {
+				background: '#cfcfcf'
+			},
+			clearButton: {
+				background: '#ffffff',
+				color: '#0000ff',
+				border: '1px solid #0000ff'
 			}
 		};
 	}
 
-	isValidInput = (wholeInput, input, prevInput) => {
+	isValidInput = (wholeInput, currentInput, prevInput) => {
 		if (
 			wholeInput.startsWith(',') ||
 			wholeInput.startsWith('-') ||
@@ -33,12 +45,12 @@ class App extends Component {
 			return false;
 		}
 		for (let i = 0; i < 10; i++) {
-			if (i.toString() === input) return true;
+			if (i.toString() === currentInput) return true;
 		}
 		if (
-			(input === ',' && prevInput !== ',') ||
-			(input === '-' && prevInput !== '-') ||
-			this.state.input.length === 0
+			(currentInput === ',' && prevInput !== ',') ||
+			(currentInput === '-' && prevInput !== '-') ||
+			wholeInput.length === 0
 		) {
 			return true;
 		}
@@ -46,11 +58,7 @@ class App extends Component {
 	};
 
 	verifyDuplicate = (number, duplicateArr, uniqueInputArr) => {
-		// console.log({ number });
-		// let duplicateArr = this.state.duplicateArr;
 		let defaultArr = this.state.defaultArr;
-		// let inputArr = [];
-		// let uniqueInputArr = [];
 		if (Array.isArray(number)) {
 			for (let i = parseInt(number[0]); i <= parseInt(number[1]); i++) {
 				i = i.toString();
@@ -65,23 +73,26 @@ class App extends Component {
 		} else if (!uniqueInputArr.includes(number)) {
 			uniqueInputArr.push(number);
 		}
-		console.log([duplicateArr, uniqueInputArr]);
 		return [duplicateArr, uniqueInputArr];
 	};
 
 	onChangeHandler = (e) => {
 		let input = e.target.value;
-		this.setState({ input });
+		if (!input.length) {
+			this.clearClickHandler();
+			return;
+		}
 		if (this.isValidInput(input, input[input.length - 1], input[input.length - 2])) {
 			this.setState({ error: false });
 			let inputArr = input.split(',');
-			console.log({ inputArr });
 			let duplicateArr = [];
 			let uniqueInputArr = [];
 			for (let i = 0; i < inputArr.length; i++) {
-				// let duplicateArr = [];
-				// let uniqueInputArr = [];
-				// let abc = [];
+				if (isNaN(inputArr[i]) && !inputArr[i].includes('-')) {
+					let error = true;
+					this.setState({ error, input });
+					return;
+				}
 				if (inputArr[i].includes('-')) {
 					let rangeInput = inputArr[i].split('-');
 					if (rangeInput.length === 2 && parseInt(rangeInput[0]) < parseInt(rangeInput[1])) {
@@ -100,24 +111,38 @@ class App extends Component {
 			}
 			duplicateArr = [...new Set(duplicateArr)];
 			uniqueInputArr = [...new Set(uniqueInputArr)];
-			console.log({ duplicateArr, uniqueInputArr });
+			this.setState({ duplicateArr, uniqueInputArr, input });
 		} else {
 			let error = true;
-			this.setState({ error });
+			this.setState({ error, input });
 		}
+	};
+
+	submitClickHandler = () => {
+		let { defaultArr, uniqueInputArr } = this.state;
+		defaultArr.push(...uniqueInputArr);
+		this.setState({ defaultArr }, () => {
+			this.clearClickHandler();
+		});
 	};
 
 	clearClickHandler = () => {
 		let input = '';
-		this.setState({ input });
+		let duplicateArr = [];
+		let uniqueInputArr = [];
+		let error = false;
+		this.setState({ input, duplicateArr, uniqueInputArr, error });
 	};
 
 	render() {
-		// console.log(this.state);
+		let { input, error, duplicateArr, defaultArr } = this.state;
+		let disableSubmit = error || !input.length;
 		return (
 			<div className="app">
 				<h1>Array Validator</h1>
-				<p>Please enter numbers seperated by commas</p>
+				<div className="sub-heading">
+					Please enter numbers seperated by commas. No characters allowed except 0-9, space, '-' , ','
+				</div>
 				<TextField
 					style={this.styles.textField}
 					label="Enter Number"
@@ -125,11 +150,23 @@ class App extends Component {
 					value={this.state.input}
 					error={this.state.error}
 				/>
+				{this.state.error && <div className="error">*Input format is wrong</div>}
 				<div className="buttons">
-					<Button disabled={this.state.error || this.state.disableButton}>Submit</Button>
-					<Button onClick={this.clearClickHandler}>Clear</Button>
+					<Button
+						style={disableSubmit ? this.styles.disabledSubmitButton : this.styles.submitButton}
+						disabled={disableSubmit}
+						onClick={this.submitClickHandler}
+					>
+						Submit
+					</Button>
+					<Button style={this.styles.clearButton} onClick={this.clearClickHandler}>
+						Clear
+					</Button>
 				</div>
-				<div>Array : {`${this.state.defaultArr}`}</div>
+				{!!duplicateArr.length && (
+					<div className="duplicate">{`Duplicate Number(s) is/are ${duplicateArr}`}</div>
+				)}
+				<div className="default">{`Default Numbers are ${defaultArr}`}</div>
 			</div>
 		);
 	}
